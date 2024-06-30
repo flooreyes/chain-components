c928c32687b953860e74fc8a7ea2975641e84507
Flooreyes <wyatt@aori.io>
2024-06-30 14:55:09 -0400

    fix build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,28 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+      'react',
+      'react-hooks',
+      'prettier'
+    ],
     extends: [
       'eslint:recommended',
       'plugin:react/recommended',
       'plugin:react-hooks/recommended',
       'plugin:@typescript-eslint/recommended',
-      'plugin:@next/next/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-      'react-hooks',
-      '@typescript-eslint',
-      '@next/next',
+      'prettier'
     ],
     rules: {
-      'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/ban-ts-comment': 'warn',
-      'react/prop-types': 'warn',
-      'react/no-unescaped-entities': 'warn',
-      '@next/next/no-img-element': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'prettier/prettier': 'warn'
     },
     settings: {
       react: {
-        version: 'detect',
-      },
-    },
+        version: 'detect'
+      }
+    }
   };
   
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+                <link rel="icon" href="/favicon.png" sizes="any" />
+                <meta
+                    name="description"
+                    content="Blockchain icons resource for cracked frontend devs"
+                />
+                <meta
+                    name="keywords"
+                    content="React, Next, Icons, Icon Library, Crypto, Blockchains, Chains, Chain Configs"
+                />
+                <meta name="author" content="@0xflooreyes" />
+                <title>chainicons by @0xflooreyes</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
